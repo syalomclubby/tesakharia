25897098e736de11e73c7643c53d22ee24a0ee3b
syalomclubby <vitosukalodon@gmail.com>
2023-04-04 11:04:52 +0700

    update header

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function reveal() {
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
     } else {
-      reveals[i].classList.remove("active");
+      // reveals[i].classList.remove("active");
     }
   }
 }

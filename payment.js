9c56b83fc3addd56d840a3a1d83eca1bf726c886
syalomclubby <vitosukalodon@gmail.com>
2023-04-18 10:35:59 +0700

    payment js

---
+++ payment.js
@@ -0,0 +1,24 @@
+function pay1(img) {
+    document.querySelector('.paypal-img').src = img;
+  }
+  function pay1Out (imgout) {
+    document.querySelector('.paypal-img').src = imgout;
+  }
+  function pay2(img2) {
+    document.querySelector('.gopay-img').src = img2;
+  }
+  function pay2Out (img2out) {
+    document.querySelector('.gopay-img').src = img2out;
+  }
+  function pay3(img3) {
+    document.querySelector('.shopee-img').src = img3;
+  }
+  function pay3Out (img3out) {
+    document.querySelector('.shopee-img').src = img3out;
+  }
+  function pay4(img4) {
+    document.querySelector('.ovo-img').src = img4;
+  }
+  function pay4Out (img4out) {
+    document.querySelector('.ovo-img').src = img4out;
+  }
--- script.js
+++ script.js
@@ -90,28 +90,3 @@ let logoprofil = document.querySelector('.logoprofil');
 logoprofil.onclick = function(){
   logoprofil.classList.toggle('activatee');
 }
-
-function pay1(img) {
-  document.querySelector('.paypal-img').src = img;
-}
-function pay1Out (imgout) {
-  document.querySelector('.paypal-img').src = imgout;
-}
-function pay2(img2) {
-  document.querySelector('.gopay-img').src = img2;
-}
-function pay2Out (img2out) {
-  document.querySelector('.gopay-img').src = img2out;
-}
-function pay3(img3) {
-  document.querySelector('.shopee-img').src = img3;
-}
-function pay3Out (img3out) {
-  document.querySelector('.shopee-img').src = img3out;
-}
-function pay4(img4) {
-  document.querySelector('.ovo-img').src = img4;
-}
-function pay4Out (img4out) {
-  document.querySelector('.ovo-img').src = img4out;
-}
